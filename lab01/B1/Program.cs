fbc326559117b7c1f54197746ecc1879d4f429ac
Hảo Ngân <161927466+HaoNgan@users.noreply.github.com>
2025-04-19 16:29:16 +0700

    Add files via upload

---
+++ lab01/B1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cấu hình để hiển thị được tiếng Việt
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // 1. Khai báo biến
+            string ten;
+            int tuoi;
+
+            // 2. Nhập dữ liệu từ bàn phím
+            Console.Write("Nhập tên của bạn: ");
+            ten = Console.ReadLine();
+
+            Console.Write("Nhập tuổi của bạn: ");
+            tuoi = int.Parse(Console.ReadLine());
+
+            // 3. Hiển thị câu chào
+            Console.WriteLine($"Xin chào {ten}, bạn {tuoi} tuổi!");
+        }
+    }
+}
